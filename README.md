3d12c27e8ba70d40501aa26368a1064078438df7
Sean Swanson <sean-swan@hotmail.com>
2019-01-22 06:23:15 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Platformy Onion
+
+## A PLATFORMING phenom...
+
+Control the Onion.
+Be the Onion.
+Direct Onion to star.
+Keep Onion safe.
+
+_Good Luck!_
+
+### Tech
+
+ * JavaScript / ES6 - Babelized
+ * HTML
+ * CSS
+ * Phaser.js 
+
+ > Made by [Sean Swanson](http://seancswanson.com) 
---
+++ server.js
@@ -0,0 +1,15 @@
+// init project
+const express = require('express');
+const app = express();
+
+const port = 9900;
+
+app.use(express.static('public'));
+
+app.get('/', function(request, response) {
+  response.sendFile(__dirname + '/src/index.html');
+});
+
+const listener = app.listen(port, function() {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
