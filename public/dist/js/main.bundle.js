d22bd6d09d8de0cf3f1f53d92aea96e0f1a81498
Sean Swanson <sean-swan@hotmail.com>
2019-01-22 10:27:29 -0800

    Created Onion class to house the Phaser.game instance. Now have a Phaser container!

--- public/dist/js/main.bundle.js
+++ public/dist/js/main.bundle.js
@@ -1,3 +1,39 @@
 "use strict";
 
-console.log('yoooo2');
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+(function () {
+  // ----------  
+  var Onion = // ----------  
+  function Onion() {
+    _classCallCheck(this, Onion);
+
+    var config = {
+      type: Phaser.AUTO,
+      width: window.innerWidth,
+      height: window.innerHeight - 20,
+      scene: {
+        preload: preload,
+        create: create,
+        update: update
+      }
+    };
+    var game = new Phaser.Game(config); // ----------  
+
+    function preload() {
+      console.log('preload');
+    } // ----------  
+
+
+    function create() {
+      console.log('create');
+    } // ----------  
+
+
+    function update() {
+      console.log('update');
+    }
+  };
+
+  var OnionGame = new Onion();
+})();
--- public/src/js/client.js
+++ public/src/js/client.js
@@ -1,1 +1,39 @@
-console.log('yoooo2');
+(function() {
+
+  // ----------  
+  class Onion {
+    // ----------  
+    constructor() {
+      const config = {
+        type: Phaser.AUTO,
+        width: window.innerWidth,
+        height: window.innerHeight - 20,
+        scene: {
+          preload,
+          create,
+          update
+        }
+      };
+        
+      const game = new Phaser.Game(config);
+
+      // ----------  
+      function preload() {
+        console.log('preload');
+      }
+
+      // ----------  
+      function create() {
+        console.log('create');
+      }
+
+      // ----------  
+      function update() {
+        console.log('update');
+      }
+    }
+
+  }
+
+  const OnionGame = new Onion();
+})();
